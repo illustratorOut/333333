c12612998a04db8e3bc7c6201c9d10e1f6399309
Alex <bmgula55@mail.ru>
2023-11-21 00:51:22 +0300

    (

--- main/models.py
+++ main/models.py
@@ -8,12 +8,12 @@ class Autoparts(models.Model):
     art = models.CharField(max_length=150, verbose_name='Артикул')
     description = models.TextField(verbose_name='Описание')
     qty = models.IntegerField(verbose_name='Количество')
-    price = models.DecimalField(max_digits=19, decimal_places=2, verbose_name='Цена')
-    storage_location = models.CharField(max_length=100, verbose_name='Место хранения')
-    delivery_date = models.DateField(verbose_name='Дата доставки')
+    price = models.DecimalField(**NULLABLE, max_digits=19, decimal_places=2, verbose_name='Цена')
+    storage_location = models.CharField(**NULLABLE, max_length=100, verbose_name='Место хранения')
+    delivery_date = models.DateField(**NULLABLE, verbose_name='Дата доставки')
     supplier = models.TextField(verbose_name='Поставщик')
     product_is_accepted = models.BooleanField(default=False)
-    warehouse = models.CharField(max_length=200, verbose_name='Склад хранения')
+    warehouse = models.CharField(**NULLABLE, max_length=200, verbose_name='Склад хранения')
 
     def __str__(self):
         return f'{self.pk} Производитель: {self.manufacturer} {self.art}'
--- main/views.py
+++ main/views.py
@@ -17,14 +17,17 @@ def home(request):
             category_list = []
             fieldnames = ['manufacturer', 'art', 'description', 'qty', 'price', 'storage_location', 'delivery_date',
                           'supplier']
+
             with open(path, newline='') as f:
                 reader = csv.DictReader(f, delimiter=';', fieldnames=fieldnames)
                 for row in reader:
                     date_ref = datetime.datetime.strptime(str(row['delivery_date']), '%d.%m.%Y').date()
                     row['delivery_date'] = date_ref
+                    row['price'] = row['price'].replace('.', ',')
                     category_list.append(row)
 
             Autoparts.objects.bulk_create([Autoparts(**category_item) for category_item in category_list])
+
 
         try:
             handle(name_load_file)
